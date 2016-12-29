1661966ccdf58e829eda3ac061545e968191a795
vincentw09 <vincent.wijdeveld@live.nl>
2016-12-29 18:14:42 +0100

    coorect sorting of sides

--- client/app.js
+++ client/app.js
@@ -3,36 +3,59 @@
 var main = function () {
     "use strict";
 
+    $('.pickusernamemodal').modal('toggle')
+
     var messages = [];
+    var username = "";
+    var counter = 0;
     var socket = io();
-    var sendMessage = function () {
-        console.log("sumbit pressed " + $('#message').val())
-        socket.emit('chat message', $('#message').val());
+    var sendMessage = function (msg) {
+        console.log("sumbit pressed " + msg.text + " by user: " + msg.sender);
+        socket.emit('chat message', msg);
         $('#message').val('');
         return false;
     }
 
-    $('#send').click(function(){
-        sendMessage();
+    var createMessage = function () {
+        var msg = new message(
+            $('#message').val(),
+            username);
+        sendMessage(msg);
+    }
+
+    $( "#pickusername" ).submit(function( event ) {
+        username=$('#username').val();
+        console.log(username);
+        event.preventDefault();
+        $('.pickusernamemodal').modal('toggle')
     });
 
-    $('#message').on("keypress", function (event) {
-        if (event.keyCode == 13) {
-            sendMessage();
-        }
+    $( "#sendform" ).submit(function( event ) {
+        createMessage();
+
+
+        event.preventDefault();
     });
 
     socket.on('chat message', function(msg){
-        $('#chat-log').append($('<li>').text(msg));
+        console.log(msg.sender + " " + username);
+        if(msg.sender==username){
+            $('#chat-log-list').append($('<li class="bubble-own">').text(msg.text));
+        }
+        else{
+            $('#chat-log-list').append($('<li class="bubble-other">').text(msg.text));
+        }
+        $('#chat-log').scrollTop(10000);
+
+
+
     });
 
     //message constructor
-    function message(Id, Text, Date, Sender) {
-        this.ID = Id;
+    function message(Text, Sender) {
         this.text = Text;
-        this.date = Date;
         this.sender = Sender;
-    }
+    };
 
 };
 
